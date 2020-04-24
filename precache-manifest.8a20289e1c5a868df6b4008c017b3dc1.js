44b7bad359d4fd3658f2f81fe309a73679dde10c
P2risHea <henrijohan.suurorg@gmail.com>
2020-04-24 13:18:08 +0300

    Updates

---
+++ precache-manifest.8a20289e1c5a868df6b4008c017b3dc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dbea25fed5aa7709aaaf87bd72dd34a",
+    "url": "/OMX-TALLINN-INDEX/index.html"
+  },
+  {
+    "revision": "e4ec208084820c063dbf",
+    "url": "/OMX-TALLINN-INDEX/static/css/main.a62468db.chunk.css"
+  },
+  {
+    "revision": "696d9c776d5126aab1b6",
+    "url": "/OMX-TALLINN-INDEX/static/js/2.7a0ff84e.chunk.js"
+  },
+  {
+    "revision": "3e7eb0c51ce474724fe2295510ac9693",
+    "url": "/OMX-TALLINN-INDEX/static/js/2.7a0ff84e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4ec208084820c063dbf",
+    "url": "/OMX-TALLINN-INDEX/static/js/main.0b0e303c.chunk.js"
+  },
+  {
+    "revision": "6f692e1aceae9857ffda",
+    "url": "/OMX-TALLINN-INDEX/static/js/runtime-main.c4c3604b.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/OMX-TALLINN-INDEX/precache-manifest.8a20289e1c5a868df6b4008c017b3dc1.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/OMX-TALLINN-INDEX/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
